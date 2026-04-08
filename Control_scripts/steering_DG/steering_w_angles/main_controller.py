0187d94e92743560c04cdf691e5310c2deda5b63
P-franczak <franczakpawel@outlook.com>
2026-04-08 20:11:02 +0200

    Markers camera module

---
+++ Control_scripts/steering_DG/steering_w_angles/main_controller.py
@@ -0,0 +1,54 @@
+import cv2
+import socket
+import numpy as np
+from steering_logic import process_frame
+
+# --- KONFIGURACJA ---
+UDP_IP, UDP_PORT = "127.0.0.1", 8001
+LOWER_COLOR = np.array([50, 80, 50])   # Przykładowe dla błękitu
+UPPER_COLOR = np.array([255, 125, 115])
+MIN_AREA = 400
+
+# --- WYGŁADZANIE STEROWANIA ---
+smoothed_steer = 0.0
+alpha = 0.2  # Im mniejszy, tym płynniej, ale z większym opóźnieniem
+
+# Inicjalizacja sieci
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+cap = cv2.VideoCapture(0)
+
+print("System uruchomiony. Naciśnij 'q' aby wyjść.")
+
+while True:
+    ret, frame = cap.read()
+    if not ret: break
+    frame = cv2.flip(frame, 1)
+
+    # Wykorzystanie logiki z drugiego pliku
+    steer_val, mask, debug_frame = process_frame(frame, LOWER_COLOR, UPPER_COLOR, MIN_AREA)
+
+    # Wysyłka UDP
+    # Wersja z wygładzaniem:
+    smoothed_steer = (alpha * steer_val) + ((1 - alpha) * smoothed_steer)
+    # message = f"STEER:{smoothed_steer:.3f}"
+    # Wersja bez wygładzania:
+    message = f"STEER:{steer_val}"
+    sock.sendto(message.encode("utf-8"), (UDP_IP, UDP_PORT))
+
+    # Wizualizacja (Collage)
+# !!!UWAGA!!! - po testach zakomentować, by przyspieszyć działanie
+    mask_3ch = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    collage = np.hstack((debug_frame, mask_3ch))
+    cv2.putText(collage, f"STEER: {steer_val}", (20, 40), 
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+    cv2.putText(collage, f"Smoothed STEER: {smoothed_steer:.3f}", (20, 80), 
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+    
+    cv2.imshow("Sterowanie - Podgląd", collage)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+sock.close()
