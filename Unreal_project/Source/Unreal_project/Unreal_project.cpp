75e19e4ab4effdd8d3ea3e3fa4a7ca2747d58315
mezyon <wojciech.jakub.jankowski@gmail.com>
2026-04-10 17:28:14 +0200

    Added keyword detection functionality

--- Unreal_project/Source/Unreal_project/Unreal_project.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Unreal_project.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Unreal_project, "Unreal_project" );
-
-DEFINE_LOG_CATEGORY(LogUnreal_project)
